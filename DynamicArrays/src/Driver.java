c4c3394b1c1c5aa9332ad3b68154bac0ac6d9f0b
armaangoel78 <armaangoel78@gmail.com>
2016-12-03 14:12:32 -0500

    added loop to find lowest time

--- DynamicArrays/src/Driver.java
+++ DynamicArrays/src/Driver.java
@@ -4,25 +4,32 @@ public class Driver {
 	private static Random r = new Random(); 
 	
 	public static void main(String[] args) {
-		long start = System.currentTimeMillis();
-		DynamicArray arr = new DynamicArray();
-		
-		int size = 10000000;
-		int removals = 1000;
-		
-		for (int i = 0; i < size; i++) {
-			arr.add(i);
+		long lowest = 1000;
+		while (true) {
+			long start = System.currentTimeMillis();
+			DynamicArray arr = new DynamicArray();
+			
+			int size = 10000000;
+			int removals = 1000;
+			
+			for (int i = 0; i < size; i++) {
+				arr.add(i);
+			}
+			
+			for (int i = 0; i < removals; i++) {
+				int removal = r.nextInt(size);
+				arr.addToRemoveQueue(removal);
+			}
+	
+			//arr.og_remove();
+			arr.remove();
+			
+			//System.out.println(arr.compare());
+			long time = System.currentTimeMillis() - start;
+			if (time < lowest) {
+				lowest = time;
+				System.out.println(time);
+			}
 		}
-		
-		for (int i = 0; i < removals; i++) {
-			int removal = r.nextInt(size);
-			arr.addToRemoveQueue(removal);
-		}
-
-		//arr.og_remove();
-		arr.remove();
-		
-		//System.out.println(arr.compare());
-		System.out.println(System.currentTimeMillis() - start);
 	}
 }
